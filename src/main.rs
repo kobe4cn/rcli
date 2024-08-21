e5f73f4d0fcb32cef35d1993291422deaa67673f
kobe4cn <kevin.yang.xgz@gmail.com>
2024-08-21 22:21:43 +0800

    test some result

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+fn stringify(x: u32) -> String {
+    format!("error code : {x}")
+}
 fn main() {
-    println!("Hello yang, rust!");
+    let x: Result<u32, u32> = Ok(2);
+    assert_eq!(x.map_err(stringify), Ok(2));
+    let x: Result<u32, u32> = Err(13);
+    assert_eq!(x.map_err(stringify), Err("error code : 13".to_string()));
 }
