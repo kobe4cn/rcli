6fca6ff5adc15e88f92d7e55469099baaf5f25d6
kobe4cn <kevin.yang.xgz@gmail.com>
2024-08-24 23:50:47 +0800

    update the solution for fit any kind of csv header

--- src/process.rs
+++ src/process.rs
@@ -1,26 +1,32 @@
 use csv::Reader;
-use serde::{Deserialize, Serialize};
+// use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::fs;
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-struct Player {
-    name: String,
+// #[derive(Debug, Serialize, Deserialize)]
+// #[serde(rename_all = "PascalCase")]
+// struct Player {
+//     name: String,
 
-    position: String,
-    #[serde(rename = "DOB")]
-    dob: String,
+//     position: String,
+//     #[serde(rename = "DOB")]
+//     dob: String,
 
-    nationality: String,
-    #[serde(rename = "Kit Number")]
-    kit: u8,
-}
+//     nationality: String,
+//     #[serde(rename = "Kit Number")]
+//     kit: u8,
+// }
 pub fn process_csv(input: &str, output: &str) -> anyhow::Result<()> {
     let mut reader = Reader::from_path(input)?;
     let mut ret = Vec::with_capacity(128);
+    let headers = reader.headers()?.clone();
+    // let mut ret = Vec::with_capacity(128);
     // let records= reader.deserialize().map(|record| record.unwrap()).collect::<Vec<Player>>();
-    for result in reader.deserialize() {
-        let record: Player = result?;
-        ret.push(record);
+    for result in reader.records() {
+        let record = result?;
+        let json_value = headers.iter().zip(record.iter()).collect::<Value>();
+        // println!("{:?}", record);
+        // println!("{:?}", json_value);
+        ret.push(json_value);
     }
     let json = serde_json::to_string_pretty(&ret)?;
     fs::write(output, json)?;
